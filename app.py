cdf36668d5094a56fb63a0ef610841b2b43311af
Sidharth <you@example.com>
2025-09-03 16:19:40 +0530

    Added a new Home page design and redirect to static home page

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-
+from flask import render_template, request
 # Create Flask application instance
 app = Flask(__name__)
 
 # Define route for home/root URL
 @app.route('/')
 def home():
-    return '<h1>Hello</h1>'
+    return render_template('home.html')
 
 # Run the application
 if __name__ == '__main__':
